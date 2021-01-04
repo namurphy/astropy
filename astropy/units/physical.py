56386c7dc90640d0799e672599009bd3ec0b5c14
Nick Murphy <namurphy@cfa.harvard.edu>
2021-01-03 19:16:08 -0500

    Update physical types
    Parametrize and expand unit physical type tests
    
    Now pretty much all of the existing unit/physical type pairs are
    being tested in advance of the impending refactoring.  These tests
    will check backward compatibility.

--- astropy/units/physical.py
+++ astropy/units/physical.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-Defines physical unit names.
+Defines the physical types that correspond to different units.
 
 This module is not intended for use by user code directly.  Instead,
-the physical unit name of a `Unit` can be obtained using its `ptype`
-property.
+the physical unit name of a `Unit` can be obtained using its
+`physical_type` property.
 """
 
+from typing import Union, Set
 
 from . import core
 from . import si
@@ -18,11 +18,34 @@ from . import imperial
 from . import misc
 
 
-__all__ = ['def_physical_type', 'get_physical_type']
+__all__ = ["def_physical_type", "get_physical_type", "PhysicalType"]
 
 
 _physical_unit_mapping = {}
 _unit_physical_mapping = {}
+
+
+class PhysicalType:
+    """
+    Represents and provides information on the physical type(s) that are
+    associated with a set of units.
+    """
+
+    def __init__(self, unit, physical_types: Union[str, Set[str]]):
+        self._unit = unit
+        self._physical_type_id = unit._get_physical_type_id()
+        if isinstance(physical_types, str):
+            self._as_set = {physical_types}
+        elif isinstance(physical_types, set):
+            self._as_set = physical_types
+
+    @property
+    def as_set(self) -> Set[str]:
+        """Return a `set` of all physical types that represent a `Unit`."""
+        return self._as_set
+
+    def _get_physical_type_id(self):
+        return self._physical_type_id
 
 
 def def_physical_type(unit, name):
@@ -35,99 +58,98 @@ def def_physical_type(unit, name):
         The unit to map from.
 
     name : str
-        The physical name of the unit.
+        The physical type of the unit.
     """
     r = unit._get_physical_type_id()
     if r in _physical_unit_mapping:
         raise ValueError(
-            f"{r!r} ({name!r}) already defined as {_physical_unit_mapping[r]!r}")
+            f"{r!r} ({name!r}) already defined as {_physical_unit_mapping[r]!r}"
+        )
     _physical_unit_mapping[r] = name
     _unit_physical_mapping[name] = r
 
 
 def get_physical_type(unit):
     """
-    Given a unit, returns the name of the physical quantity it
-    represents.  If it represents an unknown physical quantity,
-    ``"unknown"`` is returned.
+    Given a unit, returns the name of the physical type it represents.
+    If it represents an unknown physical type, `"unknown"` is returned.
 
     Parameters
     ----------
     unit : `~astropy.units.UnitBase` instance
-        The unit to lookup
+        The unit to get the physical type for
 
     Returns
     -------
-    physical : str
-        The name of the physical quantity, or unknown if not
-        known.
+    physical_type : str
+        The name of the physical type, or `"unknown"` if not known.
     """
     r = unit._get_physical_type_id()
-    return _physical_unit_mapping.get(r, 'unknown')
+    return _physical_unit_mapping.get(r, "unknown")
 
 
 for unit, name in [
-    (core.Unit(1), 'dimensionless'),
-    (si.m, 'length'),
-    (si.m ** 2, 'area'),
-    (si.m ** 3, 'volume'),
-    (si.s, 'time'),
-    (si.rad, 'angle'),
-    (si.sr, 'solid angle'),
-    (si.m / si.s, 'speed'),
-    (si.m / si.s ** 2, 'acceleration'),
-    (si.Hz, 'frequency'),
-    (si.g, 'mass'),
-    (si.mol, 'amount of substance'),
-    (si.K, 'temperature'),
-    (si.deg_C, 'temperature'),
-    (imperial.deg_F, 'temperature'),
-    (si.N, 'force'),
-    (si.J, 'energy'),
-    (si.Pa, 'pressure'),
-    (si.W, 'power'),
-    (si.kg / si.m ** 3, 'mass density'),
-    (si.m ** 3 / si.kg, 'specific volume'),
-    (si.mol / si.m ** 3, 'molar concentration'),
-    (si.kg * si.m / si.s, 'momentum/impulse'),
-    (si.kg * si.m ** 2 / si.s, 'angular momentum'),
-    (si.rad / si.s, 'angular speed'),
-    (si.rad / si.s ** 2, 'angular acceleration'),
-    (si.g / (si.m * si.s), 'dynamic viscosity'),
-    (si.m ** 2 / si.s, 'kinematic viscosity'),
-    (si.m ** -1, 'wavenumber'),
-    (si.A, 'electrical current'),
-    (si.C, 'electrical charge'),
-    (si.V, 'electrical potential'),
-    (si.Ohm, 'electrical resistance'),
-    (si.S, 'electrical conductance'),
-    (si.F, 'electrical capacitance'),
-    (si.C * si.m, 'electrical dipole moment'),
-    (si.A / si.m ** 2, 'electrical current density'),
-    (si.V / si.m, 'electrical field strength'),
-    (si.C / si.m ** 2, 'electrical flux density'),
-    (si.C / si.m ** 3, 'electrical charge density'),
-    (si.F / si.m, 'permittivity'),
-    (si.Wb, 'magnetic flux'),
-    (si.T, 'magnetic flux density'),
-    (si.A / si.m, 'magnetic field strength'),
-    (si.H / si.m, 'electromagnetic field strength'),
-    (si.H, 'inductance'),
-    (si.cd, 'luminous intensity'),
-    (si.lm, 'luminous flux'),
-    (si.lx, 'luminous emittance/illuminance'),
-    (si.W / si.sr, 'radiant intensity'),
-    (si.cd / si.m ** 2, 'luminance'),
-    (astrophys.Jy, 'spectral flux density'),
-    (cgs.erg / si.angstrom / si.cm ** 2 / si.s, 'spectral flux density wav'),
-    (astrophys.photon / si.Hz / si.cm ** 2 / si.s, 'photon flux density'),
-    (astrophys.photon / si.AA / si.cm ** 2 / si.s, 'photon flux density wav'),
-    (astrophys.R, 'photon flux'),
-    (misc.bit, 'data quantity'),
-    (misc.bit / si.s, 'bandwidth'),
-    (cgs.Franklin, 'electrical charge (ESU)'),
-    (cgs.statampere, 'electrical current (ESU)'),
-    (cgs.Biot, 'electrical current (EMU)'),
-    (cgs.abcoulomb, 'electrical charge (EMU)')
+    (core.Unit(1), "dimensionless"),
+    (si.m, "length"),
+    (si.m ** 2, "area"),
+    (si.m ** 3, "volume"),
+    (si.s, "time"),
+    (si.rad, "angle"),
+    (si.sr, "solid angle"),
+    (si.m / si.s, "speed"),
+    (si.m / si.s ** 2, "acceleration"),
+    (si.Hz, "frequency"),
+    (si.g, "mass"),
+    (si.mol, "amount of substance"),
+    (si.K, "temperature"),
+    (si.deg_C, "temperature"),
+    (imperial.deg_F, "temperature"),
+    (si.N, "force"),
+    (si.J, "energy"),
+    (si.Pa, "pressure"),
+    (si.W, "power"),
+    (si.kg / si.m ** 3, "mass density"),
+    (si.m ** 3 / si.kg, "specific volume"),
+    (si.mol / si.m ** 3, "molar concentration"),
+    (si.kg * si.m / si.s, "momentum/impulse"),
+    (si.kg * si.m ** 2 / si.s, "angular momentum"),
+    (si.rad / si.s, "angular speed"),
+    (si.rad / si.s ** 2, "angular acceleration"),
+    (si.g / (si.m * si.s), "dynamic viscosity"),
+    (si.m ** 2 / si.s, "kinematic viscosity"),
+    (si.m ** -1, "wavenumber"),
+    (si.A, "electrical current"),
+    (si.C, "electrical charge"),
+    (si.V, "electrical potential"),
+    (si.Ohm, "electrical resistance"),
+    (si.S, "electrical conductance"),
+    (si.F, "electrical capacitance"),
+    (si.C * si.m, "electrical dipole moment"),
+    (si.A / si.m ** 2, "electrical current density"),
+    (si.V / si.m, "electrical field strength"),
+    (si.C / si.m ** 2, "electrical flux density"),
+    (si.C / si.m ** 3, "electrical charge density"),
+    (si.F / si.m, "permittivity"),
+    (si.Wb, "magnetic flux"),
+    (si.T, "magnetic flux density"),
+    (si.A / si.m, "magnetic field strength"),
+    (si.H / si.m, "electromagnetic field strength"),
+    (si.H, "inductance"),
+    (si.cd, "luminous intensity"),
+    (si.lm, "luminous flux"),
+    (si.lx, "luminous emittance/illuminance"),
+    (si.W / si.sr, "radiant intensity"),
+    (si.cd / si.m ** 2, "luminance"),
+    (astrophys.Jy, "spectral flux density"),
+    (cgs.erg / si.angstrom / si.cm ** 2 / si.s, "spectral flux density wav"),
+    (astrophys.photon / si.Hz / si.cm ** 2 / si.s, "photon flux density"),
+    (astrophys.photon / si.AA / si.cm ** 2 / si.s, "photon flux density wav"),
+    (astrophys.R, "photon flux"),
+    (misc.bit, "data quantity"),
+    (misc.bit / si.s, "bandwidth"),
+    (cgs.Franklin, "electrical charge (ESU)"),
+    (cgs.statampere, "electrical current (ESU)"),
+    (cgs.Biot, "electrical current (EMU)"),
+    (cgs.abcoulomb, "electrical charge (EMU)"),
 ]:
     def_physical_type(unit, name)
--- astropy/units/tests/test_physical.py
+++ astropy/units/tests/test_physical.py
@@ -1,9 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-Regression tests for the physical_type support in the units package
+Unit tests for the physical_type support in the units package
 """
 
+import pytest
 
 from astropy import units as u
 from astropy.units import physical
@@ -11,50 +12,99 @@ from astropy.constants import hbar
 from astropy.tests.helper import raises
 
 
-def test_simple():
-    assert u.m.physical_type == 'length'
+unit_physical_type_pairs = [
+    (u.m, "length"),
+    (u.cm ** 3, "volume"),
+    (u.km / u.h, "speed"),
+    (u.barn * u.Mpc, "volume"),
+    (u.m * u.s ** 8, "unknown"),
+    (u.m / u.m, "dimensionless"),
+    (hbar.unit, "angular momentum"),
+    (u.erg / (u.cm ** 2 * u.s * u.AA), "spectral flux density wav"),  # flam
+    (u.photon / (u.cm ** 2 * u.s * u.AA), "photon flux density wav"),  # photlam
+    (u.photon / (u.cm ** 2 * u.s * u.Hz), "photon flux density"),  # photnu
+    (u.byte, "data quantity"),
+    (u.bit, "data quantity"),
+    (u.imperial.mi / u.week, "speed"),
+    (u.erg / u.s, "power"),
+    (u.C / u.s, "electrical current"),
+    (u.C / u.s / u.cm ** 2, "electrical current density"),
+    (u.T * u.m ** 2, "magnetic flux"),
+    (u.N * u.m, "energy"),
+    (u.rad / u.ms, "angular speed"),
+    (u.Unit(1), "dimensionless"),
+    (u.m ** 2, "area"),
+    (u.s, "time"),
+    (u.rad, "angle"),
+    (u.sr, "solid angle"),
+    (u.m / u.s ** 2, "acceleration"),
+    (u.Hz, "frequency"),
+    (u.g, "mass"),
+    (u.mol, "amount of substance"),
+    (u.K, "temperature"),
+    (u.deg_C, "temperature"),
+    (u.imperial.deg_F, "temperature"),
+    (u.N, "force"),
+    (u.J, "energy"),
+    (u.Pa, "pressure"),
+    (u.W, "power"),
+    (u.kg / u.m ** 3, "mass density"),
+    (u.m ** 3 / u.kg, "specific volume"),
+    (u.mol / u.m ** 3, "molar concentration"),
+    (u.kg * u.m / u.s, "momentum/impulse"),
+    (u.kg * u.m ** 2 / u.s, "angular momentum"),
+    (u.rad / u.s, "angular speed"),
+    (u.rad / u.s ** 2, "angular acceleration"),
+    (u.g / (u.m * u.s), "dynamic viscosity"),
+    (u.m ** 2 / u.s, "kinematic viscosity"),
+    (u.m ** -1, "wavenumber"),
+    (u.A, "electrical current"),
+    (u.C, "electrical charge"),
+    (u.V, "electrical potential"),
+    (u.Ohm, "electrical resistance"),
+    (u.S, "electrical conductance"),
+    (u.F, "electrical capacitance"),
+    (u.C * u.m, "electrical dipole moment"),
+    (u.A / u.m ** 2, "electrical current density"),
+    (u.V / u.m, "electrical field strength"),
+    (u.C / u.m ** 2, "electrical flux density"),
+    (u.C / u.m ** 3, "electrical charge density"),
+    (u.F / u.m, "permittivity"),
+    (u.Wb, "magnetic flux"),
+    (u.T, "magnetic flux density"),
+    (u.A / u.m, "magnetic field strength"),
+    (u.H / u.m, "electromagnetic field strength"),
+    (u.H, "inductance"),
+    (u.cd, "luminous intensity"),
+    (u.lm, "luminous flux"),
+    (u.lx, "luminous emittance/illuminance"),
+    (u.W / u.sr, "radiant intensity"),
+    (u.cd / u.m ** 2, "luminance"),
+    (u.astrophys.Jy, "spectral flux density"),
+    (u.astrophys.R, "photon flux"),
+    (u.misc.bit, "data quantity"),
+    (u.misc.bit / u.s, "bandwidth"),
+    (u.cgs.Franklin, "electrical charge (ESU)"),
+    (u.cgs.statampere, "electrical current (ESU)"),
+    (u.cgs.Biot, "electrical current (EMU)"),
+    (u.cgs.abcoulomb, "electrical charge (EMU)"),
+]
 
 
-def test_power():
-    assert (u.cm ** 3).physical_type == 'volume'
-
-
-def test_speed():
-    assert (u.km / u.h).physical_type == 'speed'
-
-
-def test_unknown():
-    assert (u.m * u.s).physical_type == 'unknown'
-
-
-def test_dimensionless():
-    assert (u.m / u.m).physical_type == 'dimensionless'
-
-
-def test_angular_momentum():
-    assert hbar.unit.physical_type == 'angular momentum'
-
-
-def test_flam():
-    flam = u.erg / (u.cm**2 * u.s * u.AA)
-    assert flam.physical_type == 'spectral flux density wav'
-
-
-def test_photlam():
-    photlam = u.photon / (u.cm ** 2 * u.s * u.AA)
-    assert photlam.physical_type == 'photon flux density wav'
-
-
-def test_photnu():
-    photnu = u.photon / (u.cm ** 2 * u.s * u.Hz)
-    assert photnu.physical_type == 'photon flux density'
+@pytest.mark.parametrize("unit, physical_type", unit_physical_type_pairs)
+def test_physical_types(unit, physical_type):
+    """
+    Test that the `physical_type` attribute of `Unit` objects provides
+    the expected physical type for various units.
+    """
+    if unit.physical_type != physical_type:
+        pytest.fail(
+            f"{repr(unit)}.physical_type was expected to return "
+            f"{repr(physical_type)}, but instead returned "
+            f"{unit.physical_type}."
+        )
 
 
 @raises(ValueError)
 def test_redundant_physical_type():
-    physical.def_physical_type(u.m, 'utter craziness')
-
-
-def test_data_quantity():
-    assert u.byte.physical_type == 'data quantity'
-    assert u.bit.physical_type == 'data quantity'
+    physical.def_physical_type(u.m, "redundant")
